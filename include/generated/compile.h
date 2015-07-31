bad90f546355000070e99e7d51675537366e9195
assusdan <assusdan@yandex.ru>
2015-07-31 14:38:04 +0000

    init

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Jul 31 11:30:37 UTC 2015"
+#define LINUX_COMPILE_BY "Android-build"
+#define LINUX_COMPILE_HOST "HP-ProLiant-DL580"
+#define LINUX_COMPILER "gcc version 4.9.2 20140811 (prerelease) (Linaro GCC 4.9-2014.08) "
