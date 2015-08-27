851790ec592801bfbb36cf1dfc01ea7feeff48dd
assusdan <assusdan1@gmail.com>
2015-08-27 10:41:26 +0000

    Dispsys from eastaeon Source

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 17 */
+/* This file is auto generated, version 18 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#17 SMP PREEMPT Wed Aug 26 06:55:25 UTC 2015"
+#define UTS_VERSION "#18 SMP PREEMPT Thu Aug 27 10:39:40 UTC 2015"
 #define LINUX_COMPILE_BY "Android-build"
 #define LINUX_COMPILE_HOST "HP-ProLiant-DL580"
 #define LINUX_COMPILER "gcc version 4.9.3 20150311 (prerelease) (Linaro GCC 4.9-2015.03) "
