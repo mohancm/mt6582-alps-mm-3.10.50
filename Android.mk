654d12573992a49696ebba34c9dd07d05306b2e5
assusdan <assusdan@yandex.ru>
2015-10-03 12:52:19 +0300

    Up

--- Android.mk
+++ Android.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2009-2011 The Android-x86 Open Source Project
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
