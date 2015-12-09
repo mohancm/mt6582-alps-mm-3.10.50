a2f3e5b930ba9907c62ff3aa6fd889b0425d97d1
Anomalchik <vidslava@yandex.ru>
2015-12-09 16:53:24 +0300

    initial

---
+++ dirtyb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ARCH=arm
+export SUBARCH=arm
+export KBUILD_BUILD_USER="anomalchik"
+export KBUILD_BUILD_HOST="sweetmachine"
+export CROSS_COMPILE=/home/anomalchik/tch/arm-cortex_a7-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a7-linux-gnueabihf-
+#make clean
+#make mrproper
+make alps_defconfig
+make -j4 zImage
